edbe8297460be04d361560145eb628e16b726f2e
AVGilderman <avgilderman@icloud.com>
2019-01-27 01:00:47 +0300

    Добавил в методс.рб новые методы

---
+++ Visilitsa/methods.rb
@@ -0,0 +1,36 @@
+#массив букв загаданного слова
+def get_letters
+	#брать слово из параметров запуска программы
+	slovo = ARGV[0]
+	if (slovo == nil || slovo == "")
+		abort "Для игры введите загаданное слово в качестве аргумента при запуске программы"
+		return slovo.encode("UTF-8").split("")
+end
+
+def get_user_input
+	#форматированная буква, которую ввёл пользователь
+	letter = ""
+	while letter == "" do
+		letter = STDN.gets.encode("UTF-8").chomp
+	end
+	return letter		
+end
+
+def check_result(user_input, letters, good_letters, bad_letters)
+	#возвращаемое значение:	1, если буква, которую ввёл игрок есть в слове и игрок отгадал всё слово, -1, если буквы, которую ввёл игрок, нет в загаданном слове, 0 во всех других случаях
+	if good_letters.include?(user_input) || bad_letters.include?(user_input)
+		return 0
+	end
+	if letters.include?(user_input)
+		good_letters << user_input
+	if good_letters.uniq.size == letters.uniq.size
+		return 1
+	else
+		return 0
+	end
+else
+	bad_letters << user_input
+	return -1
+end
+end
+
